39e96b193cd9827a9b45c1b7f20efa7d4cad148c
DESKTOP-SPE5PAD\Ai <khwan786@gmail.com>
2026-06-16 23:32:18 +0700

    FIX layout

--- frontend/src/app/(admin)/layout.tsx
+++ frontend/src/app/(admin)/layout.tsx
@@ -43,7 +43,7 @@ export default function AdminLayout({
         <Backdrop />
         {/* Main Content Area */}
         <div
-          className={`flex-1 transition-all  duration-300 ease-in-out ${mainContentMargin}`}
+          className={`min-w-0 w-full overflow-x-hidden transition-all duration-300 ease-in-out ${mainContentMargin}`}
         >
           {/* Header */}
           <AppHeader />
